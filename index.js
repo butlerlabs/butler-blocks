a57ee36eb653d50a527529b13c6387df2fe268aa
Jacob Green <jacob.green@butlerlabs.ai>
2022-08-03 20:00:50 -0700

    remove my api key

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ import { loadButlerBlocks } from "./src/butlerBlocks";
  * (could be used in their application, or to power a Butler API label endpoint).
  */
 
-const butlerBlocks = loadButlerBlocks('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDEwNzkyNDA4OTUyMDc4OTMxOTQyNyIsImVtYWlsIjoiamFjb2IuZ3JlZW5AYnV0bGVybGFicy5haSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE2MjEyNzg2OTM5MjJ9.io7TqbbqBrT3qXCaEgBsDZ0DcQj58HIZZD8LLVMPiZw');
+const butlerBlocks = loadButlerBlocks('MY_API_KEY');
 
 const stubData = {
-  modelId: '09f2067f-c5ee-4fef-ac87-411cb8a24ef2',
-  documentId: '9747f61d-0a4c-40e3-99eb-77b4ac389bba',
+  modelId: 'MY_MODEL_ID',
+  documentId: 'MY_DOCUMENT_ID',
 };
 
 const onSaveCallback = (docInfo) => {
